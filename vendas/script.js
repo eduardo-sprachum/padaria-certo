d5d03bad6766fd278bf38969eaa7e84b8d508ccb
eduardo-sprachum <145134393+eduardo-sprachum@users.noreply.github.com>
2025-05-21 14:52:20 -0300

    .

---
+++ vendas/script.js
@@ -0,0 +1,40 @@
+document.getElementById("adicionarProduto").addEventListener("click", function () {
+    let container = document.getElementById("produtosContainer");
+
+    let novoProduto = document.createElement("div");
+    novoProduto.classList.add("produto-item");
+
+    let idProduto = "produto" + (container.children.length + 1);
+    let idQuantidade = "quantidade" + (container.children.length + 1);
+
+    novoProduto.innerHTML = `
+        <label for="${idProduto}">Produto:</label>
+        <select id="${idProduto}" name="produto[]" required>
+            <option value="nenhum" selected disabled>Selecione um produto</option>
+            <option value="biscoito">Biscoito de Polvilho</option>
+            <option value="bolo-cenoura">Bolo de Cenoura</option>
+            <option value="bolo-chocolate">Bolo de Chocolate</option>
+            <option value="bolo-fuba">Bolo de Fubá</option>
+            <option value="bolo-laranja">Bolo de Laranja</option>
+            <option value="bolo-milho">Bolo de Milho</option>
+            <option value="coxinha">Coxinha</option>
+            <option value="croissant">Croissant</option>
+            <option value="cuca">Cuca</option>
+            <option value="donuts">Donuts</option>
+            <option value="empadinha">Empadinha</option>
+            <option value="esfiha">Esfiha</option>
+            <option value="pao-forma">Pão de Forma</option>
+            <option value="pao-frances">Pão Francês</option>
+            <option value="pastel">Pastel</option>
+            <option value="rosquinha">Rosquinha</option>
+            <option value="rosca-polvilho">Rosca de Polvilho</option>
+            <option value="torta-doce">Torta Doce</option>
+            <option value="torta-salgada">Torta Salgada</option>
+        </select>
+
+        <label for="${idQuantidade}">Quantidade:</label>
+        <input type="number" id="${idQuantidade}" name="quantidade[]" required>
+    `;
+
+    container.appendChild(novoProduto);
+});
